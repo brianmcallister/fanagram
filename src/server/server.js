1d992508c05219fdde51943573177d748e6757db
Brian McAllister <brian@brianmcallister.com>
2016-03-15 23:36:07 -0400

    Authenticate against the instagram api.

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,20 @@
 import path from 'path';
 import fs from 'fs';
 import express from 'express';
+import dotenv from 'dotenv';
+import instagram from 'instagram-node';
+
+dotenv.config();
 
 import devServer from 'dev_server';
 
+let api = instagram.instagram();
 let app = express();
+
+api.use({
+  client_id: process.env.CLIENT_ID,
+  client_secret: process.env.CLIENT_SECRET
+});
 
 app.set('publicPath', '/public');
 
@@ -14,6 +24,28 @@ app.use(app.get('publicPath'), express.static(path.resolve('build/assets')));
 app.get('/', (req, res) => {
   const publicDir = path.basename(app.get('publicPath'));
   res.sendFile(path.resolve(publicDir, 'index.html'));
+});
+
+app.get('/login', (req, res) => {
+  let url = api.get_authorization_url(process.env.REDIRECT_URL, {
+    scope: ['basic']
+  });
+
+  res.redirect(url);
+});
+
+app.get('/auth', (req, res) => {
+  const code = req.query.code;
+
+  api.authorize_user(code, process.env.REDIRECT_URL, (err, result) => {
+    if (err) {
+      console.log('error: ', err.body);
+      res.status(500).send('error: ', err.body);
+    } else {
+      console.log('success', result);
+      res.status(200).send('success', result);
+    }
+  });
 });
 
 app.get('/api/test', (req, res) => {
